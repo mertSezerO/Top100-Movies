7c6980717f2c260725d5f913fa1c8887e2c59d19
mertSezerO <117273151+mertSezerO@users.noreply.github.com>
2023-08-13 01:31:56 +0300

    Page implementation start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
 import ListProvider from "./listContext";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListPage from "./pages/listPage";
 
 export default function App() {
   return (
     <ListProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ListPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <ListPage />
     </ListProvider>
   );
 }
